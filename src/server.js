4d744243292a3f1fe644bc4f8b6520ea7869f999
JordaoNhanga15 <jordaonhanga7@gmail.com>
2022-11-28 00:18:16 +0100

    chore: middleware challenge

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 const { app } = require('./');
+const kleur = require('kleur');
+require('dotenv').config()
 
-app.listen(3333);
+
+app.listen(process.env.PORT, () => {
+  console.log(kleur.green(`Server is running on port ${process.env.PORT}`));
+});
